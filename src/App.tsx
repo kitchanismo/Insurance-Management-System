686a4f90f7594a40f14369e57954265bfe53e712
kitchan <cristian.betsayda@gmail.com>
2021-01-31 01:31:07 +0800

    fixing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Nav from 'components/common/MyNav'
 import MyAlert from 'components/common/MyAlert'
-import Layout from 'components/layout'
+import Layout from 'components/container'
 import { ClientProvider } from 'providers/ClientProvicer'
 
 const App: React.FC = (props) => {

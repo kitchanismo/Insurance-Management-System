b6989665a4692d808b477174bf1c7dc56e991b61
kitchanismo <cristian.betsayda@gmail.com>
2021-03-03 11:40:23 -0800

    design dashboard

---
+++ src/components/pages/Dashboard/ClientsGraph/index.tsx
@@ -0,0 +1,264 @@
+import { useEffect, useState, useContext } from 'react'
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts'
+import useTheme from '@material-ui/core/styles/useTheme'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import Typography from '@material-ui/core/Typography'
+import ExpandMore from '@material-ui/icons/ExpandMore'
+import { BranchContext } from 'providers/BranchProvider'
+import { getBranches } from 'services/branchService'
+
+export interface DataProps {
+  name: string
+  count: number
+}
+
+export interface ClientsGraphProps {
+  branches: string[]
+}
+
+const ClientsGraph: React.SFC<ClientsGraphProps> = ({ branches }) => {
+  const theme = useTheme()
+
+  const annualData: DataProps[] = [
+    {
+      name: 'Jan',
+      count: 3000,
+    },
+    {
+      name: 'Feb',
+      count: 3000,
+    },
+    {
+      name: 'Mar',
+      count: 2000,
+    },
+    {
+      name: 'Apr',
+      count: 2780,
+    },
+    {
+      name: 'May',
+      count: 1890,
+    },
+    {
+      name: 'Jun',
+      count: 2390,
+    },
+    {
+      name: 'Jul',
+      count: 3390,
+    },
+    {
+      name: 'Aug',
+      count: 3690,
+    },
+    {
+      name: 'Sep',
+      count: 3350,
+    },
+    {
+      name: 'Oct',
+      count: 3340,
+    },
+    {
+      name: 'Nov',
+      count: 350,
+    },
+    {
+      name: 'Dec',
+      count: 340,
+    },
+  ]
+
+  const monthlyData: DataProps[] = [
+    { name: '2', count: 122 },
+    { name: '3', count: 142 },
+    { name: '5', count: 102 },
+    { name: '30', count: 200 },
+  ]
+
+  const [anchorElRange, setAnchorElRange] = useState(null)
+
+  const [anchorElBranch, setAnchorElBranch] = useState(null)
+
+  const [textRange, setRange] = useState('THIS YEAR')
+
+  const [textBranch, setBranch] = useState('ALL BRANCHES')
+
+  const [titleRange, setTitleRange] = useState(
+    new Date(Date.now()).getFullYear().toString()
+  )
+
+  const [titleBranch, setTitleBranch] = useState('ALL BRANCHES')
+
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+
+  const handleClickRange = (event: any) => {
+    setAnchorElRange(event.currentTarget)
+  }
+
+  const handleClickBranch = (event: any) => {
+    setAnchorElBranch(event.currentTarget)
+  }
+
+  const handleSelectRange = (event: any) => {
+    const text = event.currentTarget?.textContent
+    setAnchorElRange(null)
+    if (!text) {
+      return
+    }
+
+    setRange(text)
+    if (text === 'THIS YEAR') {
+      setTitleRange(new Date(Date.now()).getFullYear().toString())
+    }
+    if (text === 'THIS MONTH') {
+      setTitleRange(months[new Date(Date.now()).getMonth()])
+    }
+  }
+
+  const handleSelectBranch = (event: any) => {
+    const text = event.currentTarget?.textContent
+    setAnchorElBranch(null)
+    if (!text) {
+      return
+    }
+
+    setBranch(text)
+    setTitleBranch(text)
+  }
+
+  const renderMenuItemBranch = (value: string) => {
+    return (
+      <MenuItem value={value} onClick={handleSelectBranch}>
+        {value}
+      </MenuItem>
+    )
+  }
+
+  return (
+    <>
+      <Grid container xs={12} justify='space-between'>
+        <Grid container xs={6} item>
+          <Button
+            aria-controls='simple-menu'
+            aria-haspopup='true'
+            color='default'
+            onClick={handleClickRange}
+            endIcon={<ExpandMore />}
+          >
+            {textRange}
+          </Button>
+          <Menu
+            id='simple-menu'
+            anchorEl={anchorElRange}
+            keepMounted
+            open={Boolean(anchorElRange)}
+            onClose={handleSelectRange}
+          >
+            <MenuItem value='THIS YEAR' onClick={handleSelectRange}>
+              THIS YEAR
+            </MenuItem>
+            <MenuItem value='THIS MONTH' onClick={handleSelectRange}>
+              THIS MONTH
+            </MenuItem>
+          </Menu>
+        </Grid>
+        <Grid container xs={6} item justify='flex-end'>
+          <Button
+            aria-controls='simple-menu-branch'
+            aria-haspopup='true'
+            color='default'
+            onClick={handleClickBranch}
+            endIcon={<ExpandMore />}
+          >
+            {textBranch}
+          </Button>
+          <Menu
+            id='simple-menu-branch'
+            anchorEl={anchorElBranch}
+            keepMounted
+            open={Boolean(anchorElBranch)}
+            onClose={handleSelectBranch}
+          >
+            {branches.map((branch) => renderMenuItemBranch(branch || ''))}
+          </Menu>
+        </Grid>
+      </Grid>
+
+      <ResponsiveContainer width='100%' height={200}>
+        <AreaChart
+          data={textRange === 'THIS YEAR' ? annualData : monthlyData}
+          margin={{ top: 10, right: 20, left: -20, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id='colorCount' x1='0' y1='0' x2='0' y2='1'>
+              <stop
+                offset='20%'
+                stopColor={theme.palette.secondary.main}
+                stopOpacity={0.8}
+              />
+              <stop
+                offset='95%'
+                stopColor={theme.palette.secondary.main}
+                stopOpacity={0}
+              />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey='name' />
+          <YAxis />
+          <CartesianGrid strokeDasharray='3 3' />
+          <Tooltip
+            contentStyle={{
+              borderRadius: 10,
+              backgroundColor: theme.palette.background.paper,
+              border: 0,
+            }}
+            // labelStyle={{ color: '#333' }}
+          />
+          {/* <Legend verticalAlign='bottom' height={40} /> */}
+          <Area
+            name='total'
+            type='monotone'
+            dataKey='count'
+            stroke={theme.palette.primary.main}
+            fillOpacity={1}
+            fill='url(#colorCount)'
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+      <Grid xs={12} item container alignItems='center' justify='center'>
+        <Typography component='h5' variant='subtitle1' color='textPrimary'>
+          {'A total of 3000 clients from ' + titleRange}
+        </Typography>
+      </Grid>
+    </>
+  )
+}
+
+export default ClientsGraph
--- src/components/pages/Dashboard/index.tsx
+++ src/components/pages/Dashboard/index.tsx
@@ -1,15 +1,207 @@
 import { GlobalContext } from 'providers/GlobalProvider'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+import Paper from '@material-ui/core/Paper'
 import React, { useContext, useEffect } from 'react'
+import ClientsGraph, { DataProps } from './ClientsGraph'
+import SalesGraph from './SalesGraph'
+import { BranchContext } from 'providers/BranchProvider'
+import { getBranches } from 'services/branchService'
+import MyGraph from 'components/common/MyGraph'
 
 export interface DashboardProps {}
+
+const data: DataProps[] = [
+  {
+    name: 'Jan',
+    count: 3000,
+  },
+  {
+    name: 'Feb',
+    count: 3000,
+  },
+  {
+    name: 'Mar',
+    count: 2000,
+  },
+  {
+    name: 'Apr',
+    count: 2780,
+  },
+  {
+    name: 'May',
+    count: 1890,
+  },
+  {
+    name: 'Jun',
+    count: 2390,
+  },
+  {
+    name: 'Jul',
+    count: 3390,
+  },
+  {
+    name: 'Aug',
+    count: 3690,
+  },
+  {
+    name: 'Sep',
+    count: 3350,
+  },
+  {
+    name: 'Oct',
+    count: 3340,
+  },
+  {
+    name: 'Nov',
+    count: 350,
+  },
+  {
+    name: 'Dec',
+    count: 340,
+  },
+]
 
 const Dashboard: React.SFC<DashboardProps> = () => {
   const [state, dispatch] = useContext(GlobalContext)!
 
   useEffect(() => {
     dispatch({ type: 'SET_TITLE', payload: 'Dashboard' })
+    getBranches().then((branches) => {
+      branchDispatch({ type: 'ON_LOAD_BRANCHES', payload: branches })
+    })
   }, [])
-  return <h3>Under Construction</h3>
+
+  const [branchState, branchDispatch] = useContext(BranchContext)!
+
+  const branches = branchState.branches.map((branch) => branch.name || '')
+
+  branches.push('ALL BRANCHES')
+
+  return (
+    <Grid container justify='center' xs={12}>
+      <Grid
+        style={{ padding: 5, marginBottom: 5 }}
+        container
+        xs={12}
+        direction='column'
+      >
+        <Paper elevation={1} style={{ padding: 20 }}>
+          <Grid container xs={12}>
+            <Grid alignItems='center' container direction='column' item xs={4}>
+              <Typography
+                component='h5'
+                variant='subtitle1'
+                color='textPrimary'
+              >
+                SMS
+              </Typography>
+              <Typography
+                component='h5'
+                variant='subtitle1'
+                color='textSecondary'
+              >
+                10
+              </Typography>
+            </Grid>
+            <Grid alignItems='center' container direction='column' item xs={4}>
+              <Typography
+                component='h5'
+                variant='subtitle1'
+                color='textPrimary'
+              >
+                Lapse
+              </Typography>
+              <Typography
+                component='h5'
+                variant='subtitle1'
+                color='textSecondary'
+              >
+                10
+              </Typography>
+            </Grid>
+            <Grid alignItems='center' container direction='column' item xs={4}>
+              <Typography
+                component='h5'
+                variant='subtitle1'
+                color='textPrimary'
+              >
+                Near
+              </Typography>
+              <Typography
+                component='h5'
+                variant='subtitle1'
+                color='textSecondary'
+              >
+                10
+              </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+      <Grid spacing={1} container xs={12}>
+        <Grid item xs={6} direction='column'>
+          <Paper elevation={2} style={{ padding: 20 }}>
+            <Typography component='h5' variant='subtitle1' color='textPrimary'>
+              Gross Sales
+            </Typography>
+            <Typography
+              component='h5'
+              variant='subtitle1'
+              color='textSecondary'
+            >
+              ₱ 10000
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} direction='column'>
+          <Paper elevation={2} style={{ padding: 20 }}>
+            <Typography component='h5' variant='subtitle1' color='textPrimary'>
+              Net Sales
+            </Typography>
+            <Typography
+              component='h5'
+              variant='subtitle1'
+              color='textSecondary'
+            >
+              ₱ 8000
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+      <Grid
+        style={{ padding: 5, marginTop: 5 }}
+        item
+        xs={12}
+        direction='column'
+        alignItems='center'
+      >
+        <Paper elevation={2} style={{ padding: 20 }}>
+          <Grid container xs={12} direction='column' alignItems='center'>
+            <Typography component='h5' variant='subtitle1' color='textPrimary'>
+              Total Clients
+            </Typography>
+            <Typography
+              component='h5'
+              variant='subtitle1'
+              color='textSecondary'
+            >
+              5000
+            </Typography>
+          </Grid>
+        </Paper>
+      </Grid>
+      <Divider
+        style={{ width: '100%', marginBottom: 10, marginTop: 20 }}
+      ></Divider>
+      <MyGraph title='client' data={data} branches={branches} />
+      <Divider
+        style={{ width: '100%', marginBottom: 10, marginTop: 20 }}
+      ></Divider>
+      <MyGraph title='sales' data={data} branches={branches} />
+    </Grid>
+  )
 }
 
 export default Dashboard
